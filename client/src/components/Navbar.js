69a22ee86f36068ad43142b0ef83f137cd7b5c2b
unknown <ricardocisneros197@gmail.com>
2023-12-21 20:44:28 -0800

    a

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900 navbarPricemaAI">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 ">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1">
           <Link to="/" className="flex items-center">
             <img src={LogoOnly} className='iconOnly' alt='Pricema AI Icon' />
